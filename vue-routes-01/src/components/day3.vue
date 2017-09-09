64bb5ddfa72b51cfeee0eb218ec97b5bbd7b68d9
Conrad Knapp <conrad.knapp@bc.edu>
2017-09-09 12:20:00 -0400

    finish grocery list

--- vue-routes-01/src/components/day3.vue
+++ vue-routes-01/src/components/day3.vue
@@ -1,14 +1,56 @@
 <template>
-<div id="app">
-<h3>My shopping list</h3>
-<ul>
-  <li>Coffee</li>
-    <li>Eggs</li>
-      <li>Bread</li>
-
-</ul>
-<input>
-<button>Add to list</button>
+<div id="app" class="container text-center">
+<h3>Grocery List</h3>
+<input id="itemBox" v-on:keyup.enter="addItem" />
+<button @click="addItem">Add to list</button>
+<div>
+<div class="list-group">
+  <a class="list-group-item" v-for="item in items">{{ item.text }}
+    <button @click="removeItem(i)">x</button>
+  </a>
 </div>
-
+</div>
+</div>
 </template>
+
+<script>
+
+export default({
+  data () {
+    return {
+      items: [
+        { text: 'coffee' },
+        { text: 'eggs' },
+        { text: 'bread' }
+      ]
+    }
+  },
+  methods: {
+    addItem () {
+      var input = document.getElementById('itemBox')
+      if (input.value !== '') {
+        this.items.push({
+          text: input.value
+        })
+        input.value = ''
+      }
+    },
+    removeItem (i) {
+      this.items.splice(i, 1)
+    }
+  }
+})
+</script>
+
+<style>
+
+#app .container {
+  background-color: lightblue;
+  max-width: 400px;
+}
+
+#itemBox {
+  margin-bottom: 10px;
+}
+
+</style>
